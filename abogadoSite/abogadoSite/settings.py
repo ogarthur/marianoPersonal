06e87e23aba1e859dde5e396add97972b3567096
Arturo <olmos.arthur@gmail.com>
2019-02-15 23:37:38 +0100

    fixing heroku 3

--- abogadoSite/abogadoSite/settings.py
+++ abogadoSite/abogadoSite/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR                    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+PROJECT_DIR                 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 TEMPLATE_DIR                = os.path.join(BASE_DIR,'templates')
 STATIC_DIR                  = os.path.join(BASE_DIR,'static')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
