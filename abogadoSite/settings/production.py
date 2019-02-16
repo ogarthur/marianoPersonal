6d374992e2f1a57ab6278e944e3076e84b09f442
Arturo <olmos.arthur@gmail.com>
2019-02-17 00:36:29 +0100

    no message

--- abogadoSite/settings/production.py
+++ abogadoSite/settings/production.py
@@ -1,19 +1,20 @@
 from __future__ import absolute_import, unicode_literals
+import os
 from .base import *
 import dj_database_url
-import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
 DEBUG = False
-
 DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 print("sdsd")
-env = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
+
 try:
     from .local import *
 except ImportError:
