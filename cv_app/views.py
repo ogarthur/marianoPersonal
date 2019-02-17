23c36eebb8024e3b7ea94bb33cff0503a67250d9
Arturo <olmos.arthur@gmail.com>
2019-02-17 20:13:34 +0100

    cambios de diseño, imagenes reducidas

--- cv_app/views.py
+++ cv_app/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from cv_app import user_information
 # Create your views here.
 def index(request):
-    print(user_information.test_user_data)
     data = user_information.test_user_data
-
     return render(request,'cv_app/cv.html',{'user_data':data})
