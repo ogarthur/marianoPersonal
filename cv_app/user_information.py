33a8484a9870a02e2d97d732e5295d3194e2a4f1
Arturo <olmos.arthur@gmail.com>
2019-02-17 01:44:21 +0100

    cambios en info del usuario

--- cv_app/user_information.py
+++ cv_app/user_information.py
@@ -2,7 +2,7 @@
 Diccionario con toda la informacion personal del usuario
 """
 test_user_data= {
-    'empresa':'Plantilla abogados',
+    'empresa':'Plantilla abogados|Abogado en valladolid',
     'nombre':'Juan',
     'apellidos':'Lore lipsum',
     'profesion':['abogado','mediador'],
@@ -23,17 +23,29 @@ test_user_data= {
     'telefono':'+34 000 000 000',
     'direccion':['Calle falsa numero 3',' Valladolid'],
     'redes':{'twitter':'https://twitter.com/'   ,'linkedin':'https://linkedin.com/'},
-    '':'',
-    '':'',
+
 }
 user_data = {
-    'nombre':'',
-    'apellidos':'',
-    'bio':'',
-    'email':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
+    'empresa':'Olmos de Pablos Abogados|Abogado en valladolid',
+    'nombre':'Mariano',
+    'apellidos':'Olmos de Pablos',
+    'profesion':['abogado','mediador'],
+    'bio':'Breve biografia sobre el usuario',
+    'info':['Lorem ipsum dolor sit amet consectetur adipiscing elit aptent,\
+            senectus pellentesque suspendisse blandit erat laoreet ante ut, fames cras magnis\
+            orci suscipit dapibus aenean. Luctus eros varius sociosqu semper netus augue,\
+            tempus inceptos vel aptent eleifend nulla proin, donec nam fames arcu per.\
+            Cras vivamus torquent magna sem vulputate, morbi porttitor nullam mauris sagittis ligula,\
+            dignissim curabitur cursus a.','Nostra sem aliquet sed ac enim fames eros, arcu hendrerit fringilla\
+            rutrum lectus tristique luctus nisl, quam ridiculus est urna condimentum nam.\
+            Velit hendrerit eu libero auctor porta vestibulum posuere sociosqu, porttitor\
+            habitant laoreet quis felis molestie magna interdum, congue lacus risus penatibus\
+            tempor habitasse dui. Vestibulum ullamcorper eleifend sociosqu felis pharetra urna\
+            vulputate scelerisque nullam nam orci non netus quam lobortis magnis, fames diam \
+            volutpat cum congue ad hac euismod condimentum a sociis aliquet ut senectus.'],
+    'email':'fakemail@mail.com',
+    'telefono':'+34 000 000 000',
+    'direccion':['C/San Luis 4BIS 1ºA',' Valladolid'],
+    'redes':{  'linkedin':'https://linkedin.com/'},
+
 }
